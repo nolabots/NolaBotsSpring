53bb850755ae5a989430325ce6be8816657df528
Scott Robertson <SCOTTRBRTSN@robertson-mbp.attlocal.net>
2017-01-08 14:32:03 -0600

    builds

--- src/main/java/org/usfirst/frc/team5953/robot/OI.java
+++ src/main/java/org/usfirst/frc/team5953/robot/OI.java
@@ -1,8 +1,6 @@
 
 package org.usfirst.frc.team5953.robot;
 
-import edu.wpi.first.wpilibj.RobotDrive;
-
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
@@ -17,15 +15,15 @@ public class OI {
     	
         //connect some buttons to commands
     	//here is an example
-        if (redo.xbox.getAButton()){
+        if (redo.xboxClear.getAButton()){
         	redo.driveForward();
         }
         
-        if(!redo.xbox.getAButton()){
+        if(!redo.xboxClear.getAButton()){
         	redo.makeItStop();
         }
         
-        if(redo.xbox.getRightBumper()){
+        if(redo.xboxClear.getRightBumper()){
         	redo.autonomousInit();
         	redo.autonomousPeriodic();
         }
